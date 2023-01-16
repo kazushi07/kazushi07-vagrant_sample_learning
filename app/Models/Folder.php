1da1cc2649c00ab107da69de424e400c737e5b7c
vagrant <vagrant@homestead>
2023-01-16 12:53:28 +0000

    軽微な修正

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    use HasFactory;
+	use HasFactory;
+
+	public function tasks(){
+		return $this->hasMany('App\Models\Task');
+	}
 }
